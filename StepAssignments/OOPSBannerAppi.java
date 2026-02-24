09e6c3f79e5df78a66d14240c065e9c035025efb
kudchadkarkrish <krishkudchadkar@gmail.com>
2026-02-24 10:13:58 +0530

    commit UC2

---
+++ StepAssignments/OOPSBannerAppi.java
@@ -0,0 +1,16 @@
+package StepAssignments;
+
+public class OOPSBannerAppi {
+
+    public static void main(String[] args) {
+
+        System.out.println("   ***  " + "     ***   " + "  ****** " + "      **** ");
+        System.out.println(" **   ** " + "  **   **  " + " **    ** " + "  **    **");
+        System.out.println("**     **" + " **     ** " + " **     ** " + "**      ");
+        System.out.println("**     **" + " **     ** " + " **** **" + "      ** ");
+        System.out.println("**     **" + " **     ** " + " **      " + "       **");
+        System.out.println(" **   ** " + " **    **  " + " **      " + "  **    **");
+        System.out.println("   *** " + "      ***   " + "  **      " + "    **** ");
+
+    }
+}
